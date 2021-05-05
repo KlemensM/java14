55aac33db61a17ccf0b8914a921e658c0801a825
klemens <info@klemensmorbe.de>
2021-05-06 00:12:15 +0200

    initial import;

---
+++ src/main/java/kmo/java13/NullPointerExceptionExample.java
@@ -0,0 +1,53 @@
+package kmo.java13;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class NullPointerExceptionExample {
+
+    private static final String field = null;
+
+    public static void main(String[] args) {
+        final Integer one = 1;
+        final Integer two = 2;
+        final Integer three = null;
+        final LevelOne levelOne = () -> null;
+        final LevelOne levelOneWithNPE = null;
+        final LevelTwo levelTwo = () -> null;
+        final LevelTwo levelThreeWithNPEInMethod = () -> (LevelThree) () -> {
+            final String string = null;
+            string.toString();
+        };
+
+        tryAndLog(() -> {
+            final var i = one + two + three;
+        });
+        tryAndLog(() -> levelOneWithNPE.doSomething());
+        tryAndLog(() -> levelOne.doSomething().doSomething());
+        tryAndLog(() -> levelTwo.doSomething().doSomething());
+        tryAndLog(() -> levelThreeWithNPEInMethod.doSomething().doSomething());
+        tryAndLog(() -> field.toString());
+        tryAndLog(() -> new ArrayList<>(null));
+        tryAndLog(() -> Stream.of(levelOneWithNPE).forEach(lvlOne -> lvlOne.doSomething()));
+    }
+
+    interface LevelOne {
+        LevelTwo doSomething();
+    }
+
+    interface LevelTwo {
+        LevelThree doSomething();
+    }
+
+    interface LevelThree {
+        void doSomething();
+    }
+
+    public static void tryAndLog(final Runnable runnable) {
+        try {
+            runnable.run();
+        } catch (final RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+}
